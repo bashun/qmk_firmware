5527810293b3b91c3d3cb0616a415c4d2881bc1b
BabaShunji <shunji@like.local>
2018-04-09 23:36:52 +0900

    fix typo

--- keyboards/helix/rev2/keymaps/shunji1/keymap.c
+++ keyboards/helix/rev2/keymaps/shunji1/keymap.c
@@ -138,7 +138,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
    * `-------------------------------------------------------------------------------------------------'
    */
   [_LOWER] = KEYMAP( \
-      KC_TILD, KC_EXLM, KC_AT,   KC_HASH, KC_DLR,  KC_PERC,                   LGUI(KC_L), RCTL(RALT(KC_DELT)), KC_ASTR, KC_LPRN, KC_RPRN, KC_BKSP, \
+      KC_TILD, KC_EXLM, KC_AT,   KC_HASH, KC_DLR,  KC_PERC,                   LGUI(KC_L), RCTL(RALT(KC_DELT)), KC_ASTR, KC_LPRN, KC_RPRN, KC_BSPC, \
       KC_TILD, KC_EXLM, KC_AT,   KC_HASH, KC_DLR,  KC_PERC,                   KC_CIRC,    KC_AMPR,             KC_ASTR, KC_LPRN, KC_RPRN, KC_DEL, \
       KC_PSCR, KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,                     KC_F6,      KC_UNDS,             KC_PLUS, KC_LCBR, KC_RCBR, KC_PIPE, \
       _______, KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_LPRN, KC_RPRN, KC_F12,     _______,             _______, KC_HOME, KC_END,  _______, \
